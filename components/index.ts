8a860e097e7ecde6bf763ea37f0bd3156e4144d9
Vedank Naik <vedankn01@gmail.com>
2023-06-29 15:43:06 +0530

    Added show more component

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,19 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
-export { Hero, CustomButton, Footer, NavBar };
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  ShowMore,
+};
